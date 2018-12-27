6bd9a2827c963b23ae1b79ffd94e2c93d0b74bb1
이재훈 <jaehun2841@gmail.com>
2018-12-27 23:01:39 +0900

    EHCache 예제코드 작성

--- src/main/java/com/example/springstudy/SpringStudyApplication.java
+++ src/main/java/com/example/springstudy/SpringStudyApplication.java
@@ -2,7 +2,6 @@ package com.example.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "com.example.springstudy")
 public class SpringStudyApplication {
---
+++ src/main/java/com/example/springstudy/demo4_ehcache_test/service/EHCacheController.java
@@ -0,0 +1,31 @@
+package com.example.springstudy.demo4_ehcache_test.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class EHCacheController {
+
+    @Autowired
+    private EHCacheRepository repository;
+
+    @GetMapping("/data/{id}")
+    public String getData(@PathVariable("id") int id) throws InterruptedException {
+
+        long start = System.currentTimeMillis();
+        String data = repository.getData(id);
+        long end = System.currentTimeMillis();
+
+        log.info("쿼리 수행 시간 : {}ms", end-start);
+        return data;
+    }
+
+    @GetMapping("/data/delete/{id}")
+    public void getExpireCache(@PathVariable("id") int id) {
+        repository.evictCache(id);
+    }
+}
